9b4374334635cc6a8439c9ec0399f8dea489d6b8
ctrlmelive <118881107+ctrlmelive@users.noreply.github.com>
2022-11-24 01:22:14 +0000

    Update Dead time.py

--- Dead time.py
+++ Dead time.py
@@ -2,7 +2,7 @@ thatDay = "2022-8-24"
 from datetime import datetime,timedelta
 theDay = datetime.strptime(thatDay, "%Y-%m-%d").date()
 
-# And then I push 120 days + timedelta(days=120)
+# And then I push 120 days + timedelta(days=120);
 target = theDay + timedelta(days=120)
 
 print(target)
